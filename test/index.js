3b24ec16e8130b5454dcb308f7038c3e146e24fd
Mick Ryan <mick@evilbits.io>
2016-12-19 08:12:43 -0800

    Add support for taking a buffer as an input.

--- test/index.js
+++ test/index.js
@@ -1,50 +1,101 @@
 /* globals describe, it */
 
-const getColors = require('..')
-const assert = require('assert')
+const getColors = require('..');
+const fs = require('fs');
+const assert = require('assert');
 
-describe('get-image-colors', function(){
+describe('get-image-colors', function () {
 
-  it('works on JPG images', function(done){
-    getColors(__dirname + '/fixtures/thumb.jpg', function(err, palette){
+  it('works on JPG images', function (done) {
+    getColors(__dirname + '/fixtures/thumb.jpg', function (err, palette) {
       if (err) throw err
-      assert(Array.isArray(palette))
-      assert(palette.length)
+      assert(Array.isArray(palette));
+      assert(palette.length);
+      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i));
+      done();
+    })
+  });
+
+  it('works on GIF images', function (done) {
+    getColors(__dirname + '/fixtures/thumb.gif', function (err, palette) {
+      if (err) throw err;
+      assert(Array.isArray(palette));
+      assert(palette.length);
+      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i));
+      done()
+    })
+  });
+
+  it('works on PNG images', function (done) {
+    getColors(__dirname + '/fixtures/thumb.png', function (err, palette) {
+      if (err) throw err;
+      assert(Array.isArray(palette));
+      assert(palette.length);
       assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i))
       done()
     })
-  })
+  });
 
-  it('works on GIF images', function(done){
-    getColors(__dirname + '/fixtures/thumb.gif', function(err, palette){
-      if (err) throw err
-      assert(Array.isArray(palette))
-      assert(palette.length)
-      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i))
+  it('works on SVG images', function (done) {
+    this.timeout(5000);
+    getColors(__dirname + '/fixtures/thumb.svg', function (err, palette) {
+      if (err) throw err;
+      assert(Array.isArray(palette));
+      assert(palette.length);
+      palette = palette.map(function (color) {
+        return color.hex();
+      });
+      assert(palette[0].match(/^#[0-9a-f]{3,6}$/i));
       done()
     })
-  })
+  });
 
-  it('works on PNG images', function(done){
-    getColors(__dirname + '/fixtures/thumb.png', function(err, palette){
-      if (err) throw err
-      assert(Array.isArray(palette))
-      assert(palette.length)
-      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i))
+  it('works on Buffered JPEG images', function (done) {
+    const buffer = fs.readFileSync(__dirname + '/fixtures/thumb.jpg');
+
+    getColors(buffer, 'image/jpeg', function (err, palette) {
+      if (err) throw err;
+      assert(Array.isArray(palette));
+      assert(palette.length);
+      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i));
       done()
     })
-  })
+  });
 
-  it('works on SVG images', function(done){
-    this.timeout(5000)
-    getColors(__dirname + '/fixtures/thumb.svg', function(err, palette){
-      if (err) throw err
-      assert(Array.isArray(palette))
-      assert(palette.length)
-      palette = palette.map(color => color.hex())
-      assert(palette[0].match(/^#[0-9a-f]{3,6}$/i))
+  it('works on Buffered GIF images', function (done) {
+    const buffer = fs.readFileSync(__dirname + '/fixtures/thumb.gif');
+
+    getColors(buffer, 'image/gif', function (err, palette) {
+      if (err) throw err;
+      assert(Array.isArray(palette));
+      assert(palette.length);
+      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i));
       done()
     })
-  })
+  });
 
-})
+  it('works on Buffered PNG images', function (done) {
+    const buffer = fs.readFileSync(__dirname + '/fixtures/thumb.png');
+
+    getColors(buffer, 'image/png', function (err, palette) {
+      if (err) throw err;
+      assert(Array.isArray(palette));
+      assert(palette.length);
+      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i));
+      done()
+    })
+  });
+
+  it('works on Buffered SVG images', function (done) {
+    const buffer = fs.readFileSync(__dirname + '/fixtures/thumb.svg');
+
+    getColors(buffer, 'image/svg+xml', function (err, palette) {
+      if (err) throw err;
+      assert(Array.isArray(palette));
+      assert(palette.length);
+      assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i));
+      done()
+    })
+  });
+
+});
